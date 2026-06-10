cd510ec65c693b79e7576bf9c80c3921d00149d6
agayushh <agayush088@gmail.com>
2026-06-10 19:05:40 +0530

    refactor: modernize API routes and consolidate type definitions across the project

--- app/api/user/chats/route.ts
+++ app/api/user/chats/route.ts
@@ -1,40 +1,109 @@
-import { db } from '@/db';
-import { chatsTable, userTable } from '@/db/schema/schema';
-import { auth, currentUser } from '@clerk/nextjs/server'
-import { eq } from 'drizzle-orm';
-import { NextRequest, NextResponse } from 'next/server'
+import { db } from "@/db";
+import { chatsTable } from "@/db/schema/schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import { getAuthenticatedUser } from "../../lib/cacheUser";
 
 export async function GET() {
-  const user = await currentUser();
-  if(!user){
-    return NextResponse.json({
-      message: "User not found",
-    }, {
-      status: 404
-    })
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      },
+    );
   }
-  const chats = await db.select().from(chatsTable).where(eq(chatsTable.userId, user.id));
+
+  const dbUser = await getAuthenticatedUser(userId);
+
+  if (!dbUser) {
+    return NextResponse.json(
+      {
+        message: "User not found",
+      },
+      {
+        status: 404,
+      },
+    );
+  }
   try {
-    return NextResponse.json({
-      message: "Chats fetched successfully",
-      chats: chats
-    }, {
-      status: 200
-    })
+  const allChats = await db
+    .select()
+    .from(chatsTable)
+    .where(eq(chatsTable.userId, dbUser.id));
+    return NextResponse.json(
+      {
+        message: "Chats fetched successfully",
+        chats: allChats,
+      },
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
-    return NextResponse.json({
-      message: "Error while fetching chats",
-      error: error
-    }, {
-      status: 500
-    })
+    return NextResponse.json(
+      {
+        message: "Error while fetching chats",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
   }
 }
 
 export async function POST(request: NextRequest) {
   try {
-    
+    const { title } = await request.json();
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        {
+          message: "Unauthorized",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+    const dbUser = await getAuthenticatedUser(userId);
+    if (!dbUser) {
+      return NextResponse.json(
+        {
+          message: "User not found",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    const chat = await db.insert(chatsTable).values({
+      userId: dbUser.id,
+      title: title,
+    }).returning(); 
+    return NextResponse.json(
+      {
+        message: "Chat created successfully",
+        chat: chat,
+      },
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
-    
+    return NextResponse.json(
+      {
+        message: "Error while creating chat",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
   }
 }
