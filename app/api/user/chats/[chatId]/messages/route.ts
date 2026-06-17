0ab029744b8a73a498741f1d77bc1372d72eaf26
agayushh <agayush088@gmail.com>
2026-06-17 22:04:20 +0530

    refactor: remove legacy chat UI components, memory service, and deprecated utilities to streamline codebase

--- app/api/user/chats/[chatId]/messages/route.ts
+++ app/api/user/chats/[chatId]/messages/route.ts
@@ -1,6 +1,6 @@
 import { getAuthorizedChats } from "@/app/api/lib/authChats";
 import { db } from "@/db";
-import { messagesTable } from "@/db/schema/schema";
+import { messagesTable, roles } from "@/db/schema/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -23,17 +23,62 @@ export async function GET(
     { status: 200 },
   );
 }
-export async function PUT(request: NextRequest, {params}: {params: {chatId: string}}){
-  const chat = await getAuthorizedChats({params});
-  if(chat instanceof NextResponse){
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { chatId: string } },
+) {
+  const chat = await getAuthorizedChats({ params });
+  if (chat instanceof NextResponse) {
     return chat;
   }
-  const {messageId, newContent} = await request.json();
-  const updatedMessage = await db.update(messagesTable)
-    .set({content: newContent})
+  const { messageId, newContent } = await request.json();
+  const updatedMessage = await db
+    .update(messagesTable)
+    .set({ content: newContent })
     .where(eq(messagesTable.id, messageId));
   return NextResponse.json(
     { message: "Message updated successfully", updatedMessage },
     { status: 200 },
   );
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { chatId: string } },
+) {
+  const chat = await getAuthorizedChats({ params });
+  if (chat instanceof NextResponse) {
+    return chat;
+  }
+  try {
+    const { content, message, messages, role } = await request.json();
+    const updateMessageArray = await db
+      .insert(messagesTable)
+      .values({
+        content: content || message || messages,
+        role: role,
+        chatId: params.chatId,
+      })
+      .returning();
+
+    return NextResponse.json(
+      {
+        message: "Message created successfully",
+        newMessage: updateMessageArray[0],
+      },
+      {
+        status: 201,
+      },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error while creating chat",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 }
